b6550cba558c682f0f21f9e8e96ec76bdbdf5689
saldivar <javiersaldivar1203@gmail.com>
2016-10-23 21:25:24 -0500

    SON LOS NUEVOS CAMBIOS DE ESTA APP, PERO NO SE

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-
+interface Item{
+  name:string;
+  select:boolean;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +10,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  titles=[{title1:"appSowrd!",title2:"A check List"}];
+  List=new Array();
+  name='Anybody';
+  mostrarNombre=false;
+  imgUrl="assets/img.png";
+  items=[{desc:'Element1',check:true},
+    {desc:'Element2',check:false},
+    {desc:'Element1',check:true}
+  ];
+  setName(name:string){
+    if(!this.mostrarNombre){
+        this.name='Margarita Hurtado'
+        this.mostrarNombre=true;
+        }
+        else{
+        this.mostrarNombre=false;
+        this.name=name;
+        }
+  }
+  addList(word:string){
+    this.List.push({name:word,select:false});
+  }
+  removeItem(item:Item){
+    let index=this.List.indexOf(item);
+    if(index>-1){
+      this.List.splice(index,1);
+    }
+  }
+  
 }
